d3a6516d11b923753b5aa0c13913b74a55f3ebf3
ZakirAlekperov <zakiralekperov1998@gmail.com>
2023-02-25 00:47:47 +0300

    added GameActivity.kt

--- app/src/main/java/com/example/classictetris/MainActivity.kt
+++ app/src/main/java/com/example/classictetris/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.classictetris
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.example.classictetris.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.btnNewGame.setOnClickListener(this::onBtnNewGameClick)
+    }
+
+    private fun onBtnNewGameClick(view: View){
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
     }
 }
