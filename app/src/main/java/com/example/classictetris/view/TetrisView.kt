894c54e9a8400fcaca7c0c0edba251c21d810eb1
ZakirAlekperov <zakiralekperov1998@gmail.com>
2023-02-26 17:26:50 +0300

    added view.TetrisView.kt

---
+++ app/src/main/java/com/example/classictetris/view/TetrisView.kt
@@ -0,0 +1,31 @@
+package com.example.classictetris.view
+
+import android.content.Context
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import com.example.classictetris.GameActivity
+import com.example.classictetris.models.AppModel
+
+class TetrisView:View {
+
+    private val paint = Paint()
+    private var lastMove:Long = 0
+    private var model:AppModel? = null
+    private var activity: GameActivity? = null
+    private val viewHandler = ViewHandler(this)
+    private var cellSize:Dimension = Dimension(0, 0)
+    private var frameOffset:Dimension = Dimension(0,0)
+
+    constructor(context:Context, attributeSet: AttributeSet):
+            super(context, attributeSet)
+
+    constructor(context: Context, attributeSet: AttributeSet, defStyle:Int):
+            super(context,attributeSet,defStyle)
+
+    companion object {
+        private val DELAY = 500
+        private val BLOCK_OFFSET = 2
+        private val FRAMA_OFFSET_BASE = 10
+    }
+}
