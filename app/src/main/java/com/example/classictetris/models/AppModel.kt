a824dc65d815ab2398045645b9c82e186956feeb
ZakirAlekperov <zakiralekperov1998@gmail.com>
2023-02-26 12:25:41 +0300

    added AppModel.kt

--- app/src/main/java/com/example/classictetris/models/AppModel.kt
+++ app/src/main/java/com/example/classictetris/models/AppModel.kt
@@ -1,5 +1,32 @@
 package com.example.classictetris.models
 
-class AppModel {
+import com.example.classictetris.constants.FieldConstants
+import com.example.classictetris.helper.HelperFunctions
+import com.example.classictetris.storage.AppPreferences
 
+class AppModel {
+    val score = 0
+    private var preferences:AppPreferences? = null
+
+    var currentBlock:Block? = null
+    var currentState:String = Statuses.AWAITING_START.name
+    var helperFunctions: HelperFunctions = HelperFunctions()
+
+    private var field:Array<ByteArray> = helperFunctions.array2dofByte(
+        FieldConstants.ROW_COUNT.value,
+        FieldConstants.COLUMN_COUNT.value
+    )
+
+    fun setPreferences(preferences: AppPreferences?){
+        this.preferences = preferences
+    }
+
+    fun getCellStatus(row: Int, column:Int):Byte?{
+        return field[row][column]
+    }
+    private fun setCellStatus(row:Int, column:Int, status:Byte?){
+        if(status != null){
+            field[row][column] = status
+        }
+    }
 }
