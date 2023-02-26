52c222ef76501ce06630f9b7c541ddff06f3ee7f
ZakirAlekperov <zakiralekperov1998@gmail.com>
2023-02-26 17:17:26 +0300

    AppModel.kt is complete

--- app/src/main/java/com/example/classictetris/models/AppModel.kt
+++ app/src/main/java/com/example/classictetris/models/AppModel.kt
@@ -3,6 +3,7 @@ package com.example.classictetris.models
 import android.graphics.Point
 import com.example.classictetris.constants.CellConstants
 import com.example.classictetris.constants.FieldConstants
+import com.example.classictetris.constants.Motions
 import com.example.classictetris.constants.Statuses
 import com.example.classictetris.helper.HelperFunctions
 import com.example.classictetris.storage.AppPreferences
@@ -84,6 +85,142 @@ class AppModel {
         return validTranslation(position,shape as Array<ByteArray>)
     }
 
+    fun generateField(action: String){
+        if(isGameActive()){
+            resetField()
+            var frameNumber: Int? = currentBlock?.frameNumber
+            val coordinate: Point? = Point()
+            coordinate?.x = currentBlock?.position?.x
+            coordinate?.y = currentBlock?.position?.y
 
+            when(action){
+                Motions.LEFT.name -> currentBlock?.position?.x?.minus(1)
+                Motions.RIGHT.name -> currentBlock?.position?.x?.plus(1)
+                Motions.DOWN.name -> currentBlock?.position?.y?.plus(1)
+                Motions.ROTATE.name ->{
+                    frameNumber?.plus(1)
+                    if(frameNumber != null){
+                        if(frameNumber >= currentBlock?.frameCount as Int)
+                            frameNumber = 0
+                    }
+                }
+            }
+
+            if(!moveValid(coordinate as Point, frameNumber)){
+                translateBlock(currentBlock?.position as Point , currentBlock?.frameNumber as Int)
+                if (Motions.DOWN.name == action){
+                    boostScore()
+                    persistCellData()
+                    assessField()
+                    generateNextBlock()
+                    if(!blocAdditionalPossible()){
+                        currentState = Statuses.OVER.name
+                        currentBlock = null
+                        resetField(false)
+                    }
+                }
+            }
+            else{
+                if(frameNumber != null){
+                    translateBlock(coordinate, frameNumber)
+                    currentBlock?.setState(frameNumber, coordinate)
+                }
+            }
+        }
+    }
+
+    private fun resetField(ephemeralCellOnly:Boolean = true){
+        for(i in 0 until  FieldConstants.ROW_COUNT.value){
+            (0 until FieldConstants.ROW_COUNT.value).filter {
+                !ephemeralCellOnly || field[i][it] == CellConstants.EPHERMERAL.value
+            }.forEach{
+                field[i][it] = CellConstants.EMPTY.value
+            }
+        }
+    }
+
+    private fun persistCellData(){
+        for(i in field.indices){
+            for(j in 0 until  field[i].size){
+                var status = getCellStatus(i, j)
+                if(status == CellConstants.EPHERMERAL.value){
+                    status = currentBlock?.staticValue
+                    setCellStatus(i, j, status)
+                }
+            }
+        }
+    }
+
+    private fun assessField(){
+        for(i in 0 until field.size){
+            var emptyCells = 0
+            for(j in 0 until field[i].size){
+                val status = getCellStatus(i, j)
+                val isEmpty = CellConstants.EMPTY.value == status
+                if (isEmpty)
+                    emptyCells++
+                if (emptyCells == 0)
+                    shiftRows(i)
+            }
+        }
+    }
+
+    private fun translateBlock(position: Point, frameNumber: Int){
+        synchronized(field){
+            val shape:Array<ByteArray>? = currentBlock?.getShape(frameNumber)
+            if(shape != null){
+                for (i in shape.indices){
+                    for(j in 0 until shape[i].size){
+                        val y = position.y + i
+                        val x = position.x + j
+                        if (CellConstants.EMPTY.value != shape[i][j])
+                            field[y][x] = shape[i][j]
+                    }
+                }
+            }
+        }
+    }
+
+    private fun blocAdditionalPossible():Boolean{
+        if(!moveValid(currentBlock?.position as Point, currentBlock?.frameNumber))
+            return false
+        return true
+    }
+
+    private fun shiftRows(nToRow:Int){
+        if(nToRow > 0){
+            for(j in nToRow -1 downTo 0){
+                for(m in 0 until field[j].size){
+                    setCellStatus(j+1, m, getCellStatus(j,m))
+                }
+            }
+        }
+        for(j in 0 until field[0].size){
+            setCellStatus(0, j, CellConstants.EMPTY.value)
+        }
+    }
+
+    fun startGame(){
+        if(!isGameActive()){
+            currentState = Statuses.ACTIVE.name
+            generateNextBlock()
+        }
+    }
+
+    fun restertGame(){
+        resetModel()
+        startGame()
+    }
+
+    fun endGame(){
+        score = 0
+        currentState = Statuses.OVER.name
+    }
+
+    private fun resetModel(){
+        resetField(false)
+        currentState = Statuses.AWAITING_START.name
+        score = 0
+    }
 
 }
--- app/src/main/java/com/example/classictetris/models/Field.kt
+++
@@ -1,4 +0,0 @@
-package com.example.classictetris.models
-
-class Field {
-}
